0b0e9d8fc5bbc36a0bfffa5ff5c4e3580116784d
Darryl Pogue <darryl@dpogue.ca>
2014-04-24 12:53:54 -0700

    CB-6513 - Statusbar plugin for Android is not compiling
    Signed-off-by: Shazron Abdullah <shazron@apache.org>

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -29,6 +29,7 @@ import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 
 public class StatusBar extends CordovaPlugin {
